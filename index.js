022ca05d8964cd1e833834127e474b214345e757
Goffity Corleone <goffity@gmail.com>
2023-06-11 13:56:07 +0700

    feat(index.js): add /healthz endpoint to return a 200 status code fix(package.json): add serve script to run the app The /healthz endpoint was added to return a 200 status code to indicate that the app is healthy. The serve script was added to package.json to make it easier to run the app.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
-})
+});
+app.get('/healthz',(res,req) => {
+  res.status(200);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
